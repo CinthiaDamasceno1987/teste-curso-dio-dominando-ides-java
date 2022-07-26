3e7a8c4dd8d2b849f76787746568219635df511f
CinthiaDamasceno1987 <cynthya_kat@hotmail.com>
2022-07-26 17:00:04 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,14 +1,26 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		int a = 2;
+		Gato gato = new Gato ();
+		livros livros = new livros ();
+				
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 2;
 		int b = 3;	
 	
-		System.out.println( "Hello World " + (a+b));
+		System.out.println( "Hello World " + (a+b));*/
 
 	}
-
 }
+class livros { 
+		private String nome;
+		private String npag;
+}
+
  
